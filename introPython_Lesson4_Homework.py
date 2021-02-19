048ec26ba10a8a7a239af6b19b4ad89b91d48954
PopovMichael <mp.ac.ukr@gmail.com>
2021-02-19 10:38:21 +0200

    Домашнее задание №4 выполнено

--- introPython_Lesson4_Homework.py
+++ introPython_Lesson4_Homework.py
@@ -1,11 +1,11 @@
 # 1
 # У вас есть список my_list со значениями типа int. Распечатать те значения,
 # которые больше 100. Задание выполнить с помощью цикла for.
-# my_list = [95, 105, 90, 110, 85, 115]
-# for value in my_list:
-#     if value < 100:
-#         my_list.remove(value)
-# print(my_list)
+my_list = [95, 105, 90, 110, 85, 115]
+for value in my_list:
+    if value < 100:
+        my_list.remove(value)
+print(my_list)
 
 ####################
 
@@ -13,12 +13,12 @@
 # У вас есть список my_list со значениями типа int и пустой список my_results.
 # Добавить в my_results те значения, которые больше 100.
 # Распечатать список my_results. Задание выполнить с помощью цикла for.
-# my_list = [95, 105, 90, 110, 85, 115]
-# my_results = []
-# for value in my_list:
-#     if value > 100:
-#         my_results.append(value)
-# print(my_results)
+my_list = [95, 105, 90, 110, 85, 115]
+my_results = []
+for value in my_list:
+    if value > 100:
+        my_results.append(value)
+print(my_results)
 
 ####################
 
@@ -27,13 +27,13 @@
 # Если в my_list кол-во элементов < 2, то в конец добавить значение 0.
 # Если кол-во элементов >= 2, то добавить сумму последних двух элементов.
 # Количество элементов в списке можно получить с помощью функции len(my_list).
-# my_list = [95, 105, 90, 110, 85, 115]
-# # my_list = [95]  # для быстрой проверки
-# if len(my_list) < 2:
-#     my_list.append(0)
-# else:
-#     my_list.append(my_list[-1] + my_list[-2])
-# print(my_list)
+my_list = [95, 105, 90, 110, 85, 115]
+# my_list = [95]  # для быстрой проверки
+if len(my_list) < 2:
+    my_list.append(0)
+else:
+    my_list.append(my_list[-1] + my_list[-2])
+print(my_list)
 
 ####################
 
@@ -42,21 +42,21 @@
 # Вы приводите это value к типу float и выводите результат выражения value ** -1.
 # Написать программу, которая вычисляет данное выражение и корректно
 # обрабатывает возможные исключения.
-# value = input("Введите число с точкой: ")
-# try:
-#     value = float(value)
-#     result = value ** -1
-# except (ValueError, TypeError):
-#     print("Не вверный ввод!")
-#     print("Используйте только цифровые значения.")
-#     value = 0
-#     result = 1
-# except ZeroDivisionError:
-#     print("Не вверный ввод!")
-#     print("Ноль не может быть возведен в отрицательную степень.")
-#     value = 0
-#     result = "Повторине попытку"
-# print(result)
+value = input("Введите число с точкой: ")
+try:
+    value = float(value)
+    result = value ** -1
+except (ValueError, TypeError):
+    print("Не вверный ввод!")
+    print("Используйте только цифровые значения.")
+    value = 0
+    result = 1
+except ZeroDivisionError:
+    print("Не вверный ввод!")
+    print("Ноль не может быть возведен в отрицательную степень.")
+    value = 0
+    result = 1
+print(result)
 
 ####################
 
@@ -66,13 +66,13 @@
 # будет четной. Пример. [1, 11, 20, 100]. Ответ [11, 20], потому что:
 # индекс 1 + значение 11 это 12 - четное,
 # индекс 2 + значение 20 это 22 - четное.
-# my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
-# my_results = []
-# for index, value in enumerate(my_list):
-#     new_value = index + value
-#     if not new_value % 2:
-#         my_results.append(new_value)
-# print(my_results)
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+my_results = []
+for index, value in enumerate(my_list):
+    new_value = index + value
+    if not new_value % 2:
+        my_results.append(new_value)
+print(my_results)
 
 ####################
 
@@ -83,10 +83,10 @@
 # Например для списков [1, 3] и [2, 4]:
 # (1, 2)
 # (3, 4)
-# my_list_1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-# my_list_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-# for index in range(len(my_list_1)):
-#     print(my_list_1[index], my_list_2[index])
+my_list_1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+my_list_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+for index in range(len(my_list_1)):
+    print((my_list_1[index], my_list_2[index]))
 
 ####################
 
@@ -94,8 +94,9 @@
 # У вас есть строка my_string = '0123456789'.
 # Сгенерировать целые числа (тип int) от 0 до 99 и поместить их в список.
 # Задание нужно выполнить ТОЛЬКО через цикл в цикле и приведение типов.
-# my_string = "0123456789"
-# my_list = []
-# for symbol_1 in my_string:
-#     for symbol_2 in my_string:
-#         print(symbol_1 + symbol_2)
+my_string = "0123456789"
+my_list = []
+for symbol_1 in my_string:
+    for symbol_2 in my_string:
+        my_list.append(int(symbol_1 + symbol_2))
+print(my_list)
