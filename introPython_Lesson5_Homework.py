b1add4377f66c34f7d3212b1958fde3df74b1366
PopovMichael <mp.ac.ukr@gmail.com>
2021-02-19 18:00:14 +0200

    В процессе

--- introPython_Lesson5_Homework.py
+++ introPython_Lesson5_Homework.py
@@ -1,11 +1,15 @@
 # 1
 # Дано целое число (int). Определить сколько нулей в этом числе.
-# number = 123045607890
-# zero = 0
-# count = str(number).count(str(zero))
-# print(count)
+number = 123045607890
+zero = 0
+count = str(number).count(str(zero))
+print(count)
 
 # 2
 # Дано целое число (int). Определить сколько нулей в конце этого числа.
-number = 123045607890
-zero = 0
+number = 100100010000
+count = 0
+while not number % 2:
+    count += 1
+    number //= 2
+print(count)
